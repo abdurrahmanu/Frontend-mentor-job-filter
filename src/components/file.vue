7a1a4eb3428adbae137804d1f796c3794b6c5632
Ahmed Abdulrahman <104634941+abdurrahmanu@users.noreply.github.com>
2024-02-17 10:27:47 +0100

    Update components

---
+++ src/components/file.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="w-full h-full">
+  <swiper-container
+    :slides-per-view="1"
+    navigation 
+    pagination>
+    <swiper-slide lazy="true" v-for=index in imagesLength>
+    <img loading="lazy" :src="../assets/${index}.jpeg">
+   </swiper-slide>
+  </swiper-container>
+</template>
+
+<script setup>
+  import { register } from 'swiper/element/bundle';
+  register();
+
+ const imagesLength = ref(20)
+</script>
